88665c1e761bf53f1009332a7df320a1d7237c60
kvk1495 <109151201+kvk1495@users.noreply.github.com>
2022-11-14 18:48:32 +0530

    Update sample_code.sh
    check

--- sample_code.sh
+++ sample_code.sh
@@ -4,6 +4,6 @@
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
-echo "This is example for second build automatically"
+echo "This is example for third build"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
